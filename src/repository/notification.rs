72ef80cf28b528a6af02e9d5a6e951df09b0e440
Faishal Nelwan <108632813+pesolosep@users.noreply.github.com>
2024-04-17 17:17:57 +0700

    FIX Implement unsubscribe function in Notification service.

--- src/repository/notification.rs
+++ src/repository/notification.rs
@@ -4,7 +4,6 @@ use lazy_static::lazy_static;
 
 use crate::model::notification::Notification;
 
-// Singleton of Database
 lazy_static! {
     static ref NOTIFICATIONS: RwLock<Vec<Notification>> = RwLock::new(vec![]);
 }
@@ -13,13 +12,11 @@ pub struct NotificationRepository;
 
 impl NotificationRepository {
     pub fn add(notification: Notification) -> Notification {
-        NOTIFICATIONS.write().unwrap()
-            .push(notification.clone());
+        NOTIFICATIONS.write().unwrap().push(notification.clone());
         return notification;
     }
 
     pub fn list_all_as_string() -> Vec<String> {
-        return NOTIFICATIONS.read().unwrap()
-            .iter().map(|f| format!("{}", f.clone())).collect();
+        return NOTIFICATIONS.read().unwrap().iter().map(|f| format!("{}", f.clone())).collect();
     }
 }
